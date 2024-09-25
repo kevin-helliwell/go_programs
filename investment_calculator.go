c1c674131ac3a7d1fd36656eb0ee978972302e92
Kevin Helliwell <kbh789@gmail.com>
2024-09-25 14:22:50 -0400

    alternate return syntax

--- investment_calculator.go
+++ investment_calculator.go
@@ -49,8 +49,10 @@ func outputText(text string) {
 	fmt.Print(text)
 }
 
-func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
-	fv := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
-	rfv := fv / math.Pow((1+inflationRate/100), years)
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (fv float64, rfv float64) {
+	fv = investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	rfv = fv / math.Pow((1+inflationRate/100), years)
 	return fv, rfv
+	// Alternate syntax
+	// return
 }
