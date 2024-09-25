cbaf230f7aff4d27a6bcec85d165190b45cf3053
Kevin Helliwell <kbh789@gmail.com>
2024-09-24 21:17:21 -0400

    add main.go and profit calculator

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,30 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+// import (
+// 	"fmt"
+// 	"math"
+// )
 
-func main() {
-	const inflationRate = 2.5
-	var investmentAmount float64
-	expectedReturnRate := 5.5
-	var years float64
+// func investment_calculator() {
+// 	const inflationRate = 2.5
+// 	var investmentAmount float64
+// 	expectedReturnRate := 5.5
+// 	var years float64
 
-	fmt.Print("Investment Amount: ")
-	fmt.Scan(&investmentAmount)
+// 	fmt.Print("Investment Amount: ")
+// 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Expected Return Rate: ")
-	fmt.Scan(&expectedReturnRate)
+// 	fmt.Print("Expected Return Rate: ")
+// 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Years: ")
-	fmt.Scan(&years)
+// 	fmt.Print("Years: ")
+// 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+// 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 
-	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
+// 	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+// 	fmt.Println(futureValue)
+// 	fmt.Println(futureRealValue)
 
-}
+// }
---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	profit_calculator()
+	// investment_calculator()
+}
