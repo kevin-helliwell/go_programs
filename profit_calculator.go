bba1c80f998cdcb084d44dd499d336df55a11e5a
Kevin Helliwell <kbh789@gmail.com>
2024-09-25 14:52:54 -0400

    refactor profit calculator

--- profit_calculator.go
+++ profit_calculator.go
@@ -11,17 +11,25 @@ func profit_calculator() {
 	// Calculate ratio (EBT/Profit)
 	// Output EBT, profit, and ratio
 
-	var revenue, expenses, taxRate float64
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
+	revenue := requestValue("Revenue: ")
+	expenses := requestValue("Expenses: ")
+	taxRate := requestValue("Tax Rate: ")
 
-	fmt.Println(ebt, profit, ratio)
+	ebt, profit, ratio := generateReport(revenue, expenses, taxRate)
 
+	fmt.Printf("EBT: %.1f\nProfit: %.1f\nRatio: %.1f\n", ebt, profit, ratio)
+
+}
+
+func requestValue(text string) (value float64) {
+	fmt.Print(text)
+	fmt.Scan(&value)
+	return value
+}
+
+func generateReport(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+	return ebt, profit, ratio
 }
