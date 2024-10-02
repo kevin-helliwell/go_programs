c814d8ab60d316ee08feee559b791f6ea385a87f
Kevin Helliwell <kbh789@gmail.com>
2024-10-02 19:55:47 -0400

    define user struct

--- structs/structs.go
+++ structs/structs.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type user struct {
+	firstName string
+	lastName string
+	birthDate string
+	createdAt time.Time
+}
 
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	
 
 	// ... do something awesome with that gathered data!
 
+	outputUserDetails(firstName, lastName, birthdate)
+}
+
+func outputUserDetails(firstName, lastName, birthdate string) {
+	// ...
 	fmt.Println(firstName, lastName, birthdate)
 }
 
