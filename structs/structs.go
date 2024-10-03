355b91a4a47e2b1ff54bb914692bf058aff005fb
Kevin Helliwell <kbh789@gmail.com>
2024-10-02 20:00:15 -0400

    create struct instance

--- structs/structs.go
+++ structs/structs.go
@@ -7,16 +7,24 @@ import (
 
 type user struct {
 	firstName string
-	lastName string
+	lastName  string
 	birthDate string
 	createdAt time.Time
 }
 
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
-	
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser user
+
+	appUser = user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthDate: userBirthdate,
+		createdAt: time.Now(),
+	}
 
 	// ... do something awesome with that gathered data!
 
